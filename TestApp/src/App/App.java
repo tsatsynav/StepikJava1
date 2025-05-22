5f0a30cd97e2d7467454ee1a23562750fc2aaf0b
Andrey Tsatsyn <tsatsynav@yandex.ru>
2025-05-22 11:49:05 +0300

    lesson 1 done

---
+++ TestApp/src/App/App.java
@@ -0,0 +1,33 @@
+
+package App;
+
+/**
+ *
+ * @author tsatsynav
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("A ");
+        System.out.println("a");
+        System.out.print("B ");
+        System.out.println("b");
+        System.out.print("C ");
+        System.out.println("c");
+        System.out.println("HelloWorld    Hello                  World!");
+        System.out.println("abcdef");
+        System.out.println();
+        System.out.println("ghijkl");
+        System.out.println("                  РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                                     О.Хайям");
+    }
+    
+}
