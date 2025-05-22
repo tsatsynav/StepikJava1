ee5ed6424db43c5a910dc8975a4d1852c2f1d809
Andrey Tsatsyn <tsatsynav@yandex.ru>
2025-05-22 18:19:02 +0300

    add lesson 2 start

---
+++ SJ1App1.2/src/App/App.java
@@ -0,0 +1,16 @@
+package App;
+
+/**
+ *
+ * @author tsatsyn
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
