2646ae3982e4c31076b75f6b73d9085f2180bd1e
Andrey Tsatsyn <tsatsynav@yandex.ru>
2025-05-22 08:37:24 +0300

    init commit

---
+++ TestApp/src/testapp/TestApp.java
@@ -0,0 +1,17 @@
+
+package testapp;
+
+/**
+ *
+ * @author tsatsynav
+ */
+public class TestApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
