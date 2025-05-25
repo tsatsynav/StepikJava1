a55da3d62f12da19502267252daa5de46377ebe8
Andrey Tsatsyn <tsatsynav@yandex.ru>
2025-05-25 21:34:37 +0300

    lesson 2 finished

---
+++ SJ1/src/Lesson1/Lesson1.java
@@ -0,0 +1,20 @@
+package Lesson1;
+
+/**
+ * @author tsatsyn
+ */
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("A ");
+        System.out.println("a");
+        System.out.print("B ");
+        System.out.println("b");
+        System.out.print("C ");
+        System.out.println("c");
+        System.out.println("HelloWorld    Hello                  World!");
+        System.out.println("abcdef");
+        System.out.println();
+        System.out.println("ghijkl");
+    }
+}
